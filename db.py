25d6eaa6f91d3ed4acf9261cee004776901cbdf4
yxc <mychenyixin@gmail.com>
2014-10-17 17:33:07 +0800

    0.0.1

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ import MySQLdb
 
 class DB(object):
 
-    conn = MySQLdb.connect(user="root", db="blog", passwd=yxc, host="localhost")
+    conn = MySQLdb.connect(user="root", db="blog", passwd="yxc", host="localhost")
     cursor = conn.cursor()
 
     def __init__(self):
@@ -15,7 +15,6 @@ class DB(object):
         self.bookInfo = bookInfo
         if self.bookInfo.get("book_name") & self.bookInfo.get("book_isbn"):
             if self.bookInfo.get("book_author"):
-               bookInfo = [self.bookInfo.get("book_name"), self.bookInfo.get("book_isbn"),\
+               bookInfo = [self.bookInfo.get("book_name"), self.bookInfo.get("book_isbn"), \
                            self.bookInfo.get("book_author")]
                self.cursor.execute("insert into bookInfo values %s" % bookInfo)
-
