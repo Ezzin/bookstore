3bf1c28ccd9629ed588fc469edaf3288399b300c
yxc <mychenyixin@gmail.com>
2014-10-22 17:44:20 +0800

    0.0.1

--- db.py
+++ db.py
@@ -1,20 +1,45 @@
 __author__ = 'yxc'
+#-*- coding: utf-8 -*-
 
 import MySQLdb
 
 
 class DB(object):
 
-    conn = MySQLdb.connect(user="root", db="blog", passwd="yxc", host="localhost")
+    conn = MySQLdb.connect(user="root", db="blog", passwd="yxc", host="localhost", charset="utf8")
     cursor = conn.cursor()
 
-    def __init__(self):
-        pass
+    def __init__(self, book_info):
+        self.book_info = book_info
+        print repr(self.book_info.get("book_name"))
+        print repr(self.book_info.get("book_name"))
+        self.book_values = (self.book_info.get("book_name"),
+                            self.book_info.get("book_isbn"),
+                            self.book_info.get("book_author"),
+                            self.book_info.get("book_price"))
 
-    def select(self, **bookInfo):
-        self.bookInfo = bookInfo
-        if self.bookInfo.get("book_name") & self.bookInfo.get("book_isbn"):
-            if self.bookInfo.get("book_author"):
-               bookInfo = [self.bookInfo.get("book_name"), self.bookInfo.get("book_isbn"), \
-                           self.bookInfo.get("book_author")]
-               self.cursor.execute("insert into bookInfo values %s" % bookInfo)
+    @staticmethod
+    def select(self):
+        DB.cursor.execute("select * from bookInfo")
+        data = cursor.fetchall()
+        print data
+        return data
+
+    def insert(self):
+        # book_values call 4 variable, name, isbn, author abd price
+        try:
+            DB.cursor.execute("insert into bookInfo(book_name, book_isbn, book_author, book_price) values(%s, %s, %s, %s) ",
+                          self.book_values)
+            DB.conn.commit()
+        except MySQLdb.Error,e:
+            print 'cannot '
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(user="root", db="blog", passwd="yxc", host="localhost")
+    cursor = conn.cursor()
+    print cursor.execute('select * from authors where name="yxc"')
+    data = cursor.fetchall()
+    if data:
+        for da in data:
+            print da[0], da[1], da[2]
--- run.py
+++ run.py
@@ -1,70 +1,105 @@
 __author__ = 'yxc'
+#-*- coding: utf-8 -*-
 
 import tornado.options
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
 import os
-import MySQLdb
+
+import db
 
 from tornado.options import options, define
 define("port", default="8000", help="run on this port", type=int)
 
 
 class Application(tornado.web.Application):
+
     def __init__(self):
         handler = [(r"/", MainHandler),
-                   (r"/list", ListHandler)]
+                   (r"/list", ListHandler),
+                   (r"/blog", ArticleHandler),
+                   (r"/me", MeHandler),
+                   (r"/login", LoginHandler),
+                   (r"/book",BookSelectHandler)]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
-            ui_modules={"Book": BookModule},
             debug=True,)
-        conn = MySQLdb.connect(user="root", db="blog", passwd=yxc, host="localhost")
-        cursor = conn.cursor()
         tornado.web.Application.__init__(self, handler, **settings)
 
 
 class MainHandler(tornado.web.RequestHandler):
+
     def get(self, *args, **kwargs):
-        self.render("homepage.html", title="yxc")
+        self.render("homepage.html", title="yxc", page_name="YXC")
 
     def post(self, *args, **kwargs):
         pass
 
 
 class ListHandler(tornado.web.RequestHandler):
+
     def get(self, *args, **kwargs):
-        self.render("list.html", title=False, bookname="Not yet")
+        self.render("list.html", title=False, book_name="Not yet", page_name="List")
 
     def post(self, *args, **kwargs):
-        book_name = self.get_argument("bookname")
-        self.render("list.html", book_name=book_name)
-
-
-class BookModule(tornado.web.UIModule):
-    def render(self, book):
-        return self.render_string("module/book.html", book=book)
-
-    def css_files(self):
-        return "/static/css/main/style.css"
-
-    def javascript_files(self):
-        return "/static/js/main.js"
-
-
-class BookStore(tornado.web.RequestHandler):
-    def get(self, *args, **kwargs):
         book_name = self.get_argument("book_name")
         book_isbn = self.get_argument("book_isbn")
-        book_auther = self.get_argument("book_auther")
+        book_author = self.get_argument("book_author")
         book_price = self.get_argument("book_price")
-        book_cover = self.get_argument("book_cover")
+        book_info = dict(
+            book_name=book_name,
+            book_isbn=book_isbn,
+            book_author=book_author,
+            book_price=book_price
+        )
+        inner = db.DB(book_info)
+        inner.insert()
+        self.render("list.html", book_name=book_name, page_name="List")
 
 
+class BookSelectHandler(tornado.web.RequestHandler):
+
+    def get(self, *args, **kwargs):
+        print db.DB.select()
+
+
+
+
+
+# class BookModule(tornado.web.UIModule):
+#
+#     def render(self, book):
+#         return self.render_string("module/book.html", book=book)
+#
+#     def css_files(self):
+#         return "/static/css/main/style.css"
+#
+#     def javascript_files(self):
+#         return "/static/js/main.js"
+
+
+# class BookStore(tornado.web.RequestHandler):
+#     def get(self, *args, **kwargs):
+#         book_name = self.get_argument("book_name")
+#         book_isbn = self.get_argument("book_isbn")
+#         book_author = self.get_argument("book_author")
+#         book_price = self.get_argument("book_price")
+#         book_cover = self.get_argument("book_cover")
+
+
+class ArticleHandler(tornado.web.RequestHandler):
+
+    def get(self, *args, **kwargs):
+        self.render("book.html", title="blog", page_name="Blog" )
+
+    def post(self, *args, **kwargs):
+        pass
 
 
 class BookSearch(tornado.web.RequestHandler):
+
     def get(self):
         key = self.get_argument("name")
         conn = self.application.db.test
@@ -75,6 +110,14 @@ class BookSearch(tornado.web.RequestHandler):
             book_isbn = 0
         self.render("url", book_isbn)
 
+
+class MeHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render("me.html",title="Me", page_name="About Me")
+
+class LoginHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render("login", title="Login", page_name="Login")
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
